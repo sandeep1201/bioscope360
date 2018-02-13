4e7579313a5de8b4b622b85cb6df0e0509a88dd7
Alalla, SandeepReddy <SALALLA@amfam.com>
2018-02-12 20:47:20 -0600

    adding routing

--- bioscope/src/app/app.module.ts
+++ bioscope/src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router'
+import { AppbootstrapModule } from './appbootstrap/appbootstrap.module';
+import { routes } from './app-routing';
+
 
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { EntertainmentComponent } from './entertainment/entertainment.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    EntertainmentComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppbootstrapModule,
+    routes
   ],
   providers: [],
   bootstrap: [AppComponent]
